a0db7d9a39213594eb2e978a0ec2b3f5e3e1c0d2
Ma Ming <isdom@qq.com>
2024-06-14 19:39:18 +0800

    Create pve-br.sh

---
+++ pve-br.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# MEMO: TBD
+
+echo "bootfrom script V2.0"
+if [ "$#" -ne 1 ]; then
+    echo "需要提供作为根目录的子卷的名称，usage: ./br-2.sh <subvol name>"
+    exit -1
+fi
+
+sblid=$(btrfs sub list / | awk -v sbl=$1 '$9==sbl {print $2}')
+
+echo "作为根目录启动的子卷ID为:$sblid"
+if [[ $sblid =~ ^[0-9]+$ ]]; then
+    echo "$1 为有效子卷名称"
+else
+    echo "$1 不是有效子卷名称，终止执行"
+    exit -1
+fi
+
+eid=$(lsblk -no UUID $(df -P /boot/efi | awk 'END{print $1}'))
+rid=$(lsblk -no UUID $(df -P / | awk 'END{print $1}'))
+
+# 生成17位时间戳字符串
+tm=$(date +'%Y%m%d_%H%M%S')
+rfs_path=./rfs_$tm
+mkdir $rfs_path
+
+mount -o subvolid=$sblid UUID=$rid $rfs_path
+
+# 检测该子卷是否有 boot 目录，如果不存在 boot 目录，则终止设定，避免误操作
+if [ -d "$rfs_path/boot/" ]; then
+    echo "$1 为有效的根目录子卷，继续执行设定"
+else
+    echo "$1 不是有效的根目录子卷，终止执行"
+    umount $rfs_path
+    rm -r $rfs_path
+    exit -1
+fi
+
+echo "将下次启动的 btrfs 根文件系统设置为: "$1
+echo "efi uuid=$eid rootfs uuid=$rid"
+btrfs sub set $sblid /
+btrfs sub get /
+
+for i in /sys /proc /run /dev; do mount --rbind "$i" "$rfs_path$i"; done
+
+echo "mount efi part: "$eid
+mount UUID=$eid $rfs_path/boot/efi
+
+rp=$(df -P / | awk 'END{print $1}')
+rdisk=${rp:0:${#rp}-1}
+
+chroot $rfs_path update-grub
+chroot $rfs_path grub-install $rdisk
+
+echo "change boot from "$1" success"
+umount -lf $rfs_path
+rm -r $rfs_path
