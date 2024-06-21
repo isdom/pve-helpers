260937b42483d12f258fbfce423832159eb12192
Ma Ming <isdom@qq.com>
2024-06-21 09:39:45 +0800

    Update rs-localbtrfs.sh

--- rs-localbtrfs.sh
+++ rs-localbtrfs.sh
@@ -7,28 +7,28 @@ fi
 
 # 检测还原路径是否存在
 if [ -d "$2" ]; then
-    echo "$2 还原路径存在"
+    echo "$2 目标路径存在"
 else
-    echo "$2 路径不存在，终止还原"
+    echo "$2 目标路径不存在，终止还原"
     exit -1
 fi
 
 if [ ! -d "$2/@local-btrfs" ]; then
-    echo "$2 下不存在 @local-btrfs 子卷，继续还原过程"
+    echo "$2 下不存在 @local-btrfs 子卷，继续还原"
 else
     echo "$2 下已经存在 @local-btrfs 子卷，终止还原"
     exit -1
 fi
 
 if [ ! -d "$2/@local-btrfs-ro" ]; then
-    echo "$2 下不存在 @local-btrfs-ro 子卷，继续还原过程"
+    echo "$2 下不存在 @local-btrfs-ro 子卷，继续还原"
 else
     echo "$2 下已经存在 @local-btrfs-ro 子卷，终止还原"
     exit -1
 fi
 
 lb_cnt=$(ls $1 -l | awk '$9 ~/local_btrfs.snap$/{print $9}'| wc -l)
-if [ $lb_cnt -eq 1 ];then
+if [ ${lb_cnt} -eq 1 ];then
     echo "$1 包含有效的 local_btrfs 存储"
 else
     echo "$1 不包含有效 local_btrfs 存储，终止还原"
@@ -36,8 +36,8 @@ else
 fi
 
 lb_snap=$(ls $1 -l | awk '$9 ~/local_btrfs.snap$/{print $9}')
-echo "restore $lb_snap to $2"
-btrfs receive -f $1/$lb_snap $2
+echo "restore ${lb_snap} to $2"
+btrfs receive -f $1/${lb_snap} $2
 
 if [ -d "$2/@local-btrfs-ro" ]; then
     echo "restore read-only snapshot $2/@local-btrfs-ro success"
