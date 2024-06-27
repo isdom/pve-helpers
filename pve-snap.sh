621c4704c41040c83d8498b7e593759bc9c5b1e5
Ma Ming <isdom@qq.com>
2024-06-27 18:36:56 +0800

    Update pve-snap.sh

--- pve-snap.sh
+++ pve-snap.sh
@@ -3,12 +3,12 @@
 # 生成17位时间戳字符串
 tm=$(date +'%Y%m%d_%H%M%S')
 rfs_path=./rfs_$tm
-mkdir $rfs_path
+mkdir ${rfs_path}
  
 # 获取 UUID for btrfs device
 rid=$(lsblk -no UUID $(df -P / | awk 'END{print $1}'))
 # mount FS_TREE
-mount -o subvolid=5 UUID=$rid $rfs_path
+mount -o subvolid=5 UUID=${rid} ${rfs_path}
  
 if [ $? -eq 0 ];then
     echo "mount rfs success"
