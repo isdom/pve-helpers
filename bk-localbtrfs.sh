87bd857c525dfb82b81f1133b99f059b0bdefe3a
Ma Ming <isdom@qq.com>
2024-07-07 18:38:45 +0800

    Update bk-localbtrfs.sh

--- bk-localbtrfs.sh
+++ bk-localbtrfs.sh
@@ -84,9 +84,10 @@ btrfs sub del -c ${tm}/@local-btrfs-ro
 #backup snapshot to remote
 bk_vm=${bk_path}_${tm}
 
-# btrfs sub list -o $tm | awk -v tm=$tm '$9 ~/vm-/{print $9}' | awk -v tm=$tm '{print "btrfs send -f "tm"/"$0}'
+# echo "backup all vm snapshot in ${tm} to ${bk_lb}..."
 btrfs sub list -o ${tm} | awk '$9 ~/vm-/{print $9}' | awk -v bk=${bk_vm} '{split($0,x,"/")}{print "btrfs send -f "bk"_"x[2]".snap "$0}' | bash
 btrfs sub list -o ${tm} | awk '$9 ~/vm-/{print $9}' | awk '{print "btrfs sub del -c "$0}' | bash
+# echo "backup all vm snapshot in ${tm} to ${bk_lb} success"
 
 echo "complete backup @local-btrfs to remote..."
 rm -r ${tm}
