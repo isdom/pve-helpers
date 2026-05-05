03b3483f1afaa1dc9b08b4b735ed7b7c40cbce28
Ma Ming <isdom@qq.com>
2026-05-05 19:36:00 +0800

    Update debian-bk-btrfs.sh; create snap file local

--- debian-bk-btrfs.sh
+++ debian-bk-btrfs.sh
@@ -18,6 +18,22 @@ SNAP_HOME="/mnt/backup/timeshift-btrfs/snapshots/${SNAP_DATE}/@home"
 
 echo "$SNAP_ROOT"
 echo "$SNAP_HOME"
+
+if [ -d "$SNAP_ROOT" ]; then
+    echo "@ 的备份 $SNAP_ROOT 存在"
+else
+    echo "@ 的备份 $SNAP_ROOT 不存在，终止备份"
+    exit -1
+fi
+
+if [ -d "$SNAP_HOME" ]; then
+    echo "@home 的备份 $SNAP_HOME 存在"
+else
+    echo "@home 的备份 $SNAP_HOME 不存在，终止备份"
+    exit -1
+fi
+
+
 # 检查 @ 子卷的只读属性
 RO_ROOT=$(sudo btrfs property get "$SNAP_ROOT" ro | awk -F= '{print $2}')
 echo "Root snapshot ro=$RO_ROOT"
@@ -30,18 +46,34 @@ if [ "$RO_ROOT" = "true" ]; then
     SEND_ROOT="$SNAP_ROOT"
 else
     TMP_ROOT="/mnt/backup/@-send-ro"
+    if [ -d "$TMP_ROOT" ]; then
+        echo "存在旧的只读备份快照，删除旧的只读备份 $TMP_ROOT..."
+        sudo btrfs subvolume delete "$TMP_ROOT"
+    fi
     sudo btrfs subvolume snapshot -r "$SNAP_ROOT" "$TMP_ROOT"
     SEND_ROOT="$TMP_ROOT"
 fi
 
-echo "Send @ ro snapshot: $SEND_ROOT"
 
 if [ "$RO_HOME" = "true" ]; then
     SEND_HOME="$SNAP_HOME"
 else
     TMP_HOME="/mnt/backup/@home-send-ro"
+    if [ -d "$TMP_HOME" ]; then
+        echo "存在旧的只读备份快照，删除旧的只读备份 $TMP_HOME..."
+        sudo btrfs subvolume delete "$TMP_HOME"
+    fi
     sudo btrfs subvolume snapshot -r "$SNAP_HOME" "$TMP_HOME"
     SEND_HOME="$TMP_HOME"
 fi
 
+echo "Send @ ro snapshot: $SEND_ROOT"
 echo "Send @home ro snapshot: $SEND_HOME"
+
+echo "start backup $SEND_ROOT snapshot to archive file /tmp/@.btrfs"
+sudo btrfs send "$SEND_ROOT" -f /tmp/@.btrfs
+echo "finish backup $SEND_ROOT snapshot"
+
+echo "start backup $SEND_HOME snapshot to archive file /tmp/@home.btrfs"
+sudo btrfs send "$SEND_HOME" -f /tmp/@home.btrfs
+echo "finish backup $SEND_HOME snapshot"
